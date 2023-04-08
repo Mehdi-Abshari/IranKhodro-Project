2eef916ccd7e85aa99fbc081829536a1b4353b5e
Mehdi-Abshari <absharimahdi5@gmail.com>
2023-04-08 11:46:14 +0430

    responsive page and add js code

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,20 +1,29 @@
 // --> Variables
 // Selecting li in navigation bar
-let list = document.querySelectorAll("nav ul li");
+let list = document.querySelectorAll("nav ul li"),
+  menuToggle = document.querySelector(".menuToggle"),
+  header = document.querySelector("header");
 
 // --> Events
 // Create for each to get event to li
 list.forEach((i) =>
-// Set event for li
-i.addEventListener('click', active))
+  // Set event for li
+  i.addEventListener("click", active)
+);
 
 // --> Functions
 // Function for add and remove 'active' class
 function active() {
   // Create for each to add and remove 'active' class
   list.forEach((i) =>
-  // Remove 'active' class
-   i.classList.remove("active"));
-   // Add 'active' class
-   this.classList.add('active')
+    // Remove 'active' class
+    i.classList.remove("active")
+  );
+  // Add 'active' class
+  this.classList.add("active");
+}
+// Function for add 'active' class to header
+function toggleMenu(){
+  // add 'active' to header
+  header.classList.toggle('active');
 }
